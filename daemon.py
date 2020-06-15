6a9553033bf83b8e25af0433234bbca09746edaa
Zoë F <zoe@dataleek.io>
2020-06-15 11:16:15 -0600

    Added feh as requirement

--- daemon.py
+++ daemon.py
@@ -3,36 +3,38 @@
 import sys
 import os
 import pathlib
+import random
 from pprint import pprint as pp
 import time
 import signal
+from loguru import logger
 
 import daemonize
 import yaml
 import requests
 
 
-PIDFILE = pathlib.Path('/tmp/wallpapers.pid')
-
+PIDFILE = pathlib.Path("/tmp/wallpapers.pid")
 BASE_URL = "https://wallhaven.cc/api/v1/search"
-
 DOWNLOAD_DIR = pathlib.Path().home() / ".wallpapers"
-
-if not DOWNLOAD_DIR.exists():
-    os.mkdir(DOWNLOAD_DIR)
-
-
-# TODO actually include this...?
-CONFIG = DOWNLOAD_DIR / "config.yaml"
+CONFIG = DOWNLOAD_DIR / "config.yaml"   # TODO actually include this...?
+LOGFILE = pathlib.Path('/tmp/wallpapers.log')
 
 
 def main():
+    logger.remove()  # remove default stderr
+    file_handler = logger.add(LOGFILE.open("w"), level="DEBUG")
+
+    linux_command = 'feh --bg-center '
     while True:
-        get_wallpapers()
-        time.sleep(60 * 60)    # once an hour
+        latest = get_wallpapers()
+        full_command = linux_command + str(random.choice(latest).absolute())
+        logger.info(full_command)
+        os.system(full_command)
+        time.sleep(60 * 60)  # once an hour
 
 
-def get_wallpapers():
+def get_wallpapers(how_many_to_fetch=1):
     api_key = os.environ.get("WH_KEY", None)
 
     args = {}
@@ -48,8 +50,9 @@ def get_wallpapers():
     args["ratios"] = "16x9"
     args["sorting"] = "random"
 
-    how_many_to_fetch = 2
     fetched = []
+
+    logger.info(args)
 
     r = requests.get(BASE_URL, params=args)
     data = r.json()
@@ -57,6 +60,7 @@ def get_wallpapers():
         img_path = blob["path"]
         theoretical_path = DOWNLOAD_DIR / img_path.split("/")[-1]
         if not theoretical_path.exists():
+            logger.info(f"Downloading {img_path}")
             img = requests.get(img_path)
             theoretical_path.write_bytes(img.content)
             fetched.append(theoretical_path)
@@ -68,10 +72,13 @@ def get_wallpapers():
 
 
 if __name__ == "__main__":
+    if not DOWNLOAD_DIR.exists():
+        os.mkdir(DOWNLOAD_DIR)
+
     if PIDFILE.exists():
-        print('Daemon OFF')
+        print("Daemon OFF")
         os.kill(int(PIDFILE.read_text()), signal.SIGINT)
     else:
-        print('Daemon ON')
-        daemon = daemonize.Daemonize(app='wallpapers', pid=PIDFILE, action=main)
+        print("Daemon ON")
+        daemon = daemonize.Daemonize(app="wallpapers", pid=PIDFILE, action=main)
         daemon.start()
